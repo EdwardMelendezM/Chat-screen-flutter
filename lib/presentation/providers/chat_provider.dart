e2805bf10c372544214d9aaced7999f4d2bf7bed
EdwardMelendezM <192666@unsaac.edu.pe>
2023-06-12 18:25:29 -0500

    adding http request for api

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:yesnoo/domain/entities/message.dart';
+import 'package:yesnoo/helpers/get_yes_no_answer.dart';
 
 class ChatProvider extends ChangeNotifier{
 
   final ScrollController chatScrollController = ScrollController();
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
 
   List<Message> messageList = [
     Message(text: 'Hola amor', fromWho: FromWho.me),
     Message(text: 'Ya regresaste el trabajo?', fromWho: FromWho.me),
   ];
+  Future<void> herReply() async {
+    final herMesessage = await getYesNoAnswer.getAnswer();
+  }
 
   Future<void> sendMessage(String text) async{
     if(text.isEmpty) return;
     final newMessage = Message(text: text,fromWho: FromWho.me);
     messageList.add(newMessage);
+    if(text.endsWith('?')){
+      herReply();
+    }
     //Llamamos a setState pero con otro nombre
     notifyListeners();
     moveScrollToBottom();
