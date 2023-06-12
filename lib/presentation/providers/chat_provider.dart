81931fa2356b61815759d074af20622fff608b84
EdwardMelendezM <192666@unsaac.edu.pe>
2023-06-12 18:02:18 -0500

    adding scroll to run bottom for new messages

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,15 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:yesnoo/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier{
+
+  final ScrollController chatScrollController = ScrollController();
+
   List<Message> messageList = [
     Message(text: 'Hola amor', fromWho: FromWho.me),
     Message(text: 'Ya regresaste el trabajo?', fromWho: FromWho.me),
   ];
 
   Future<void> sendMessage(String text) async{
+    if(text.isEmpty) return;
     final newMessage = Message(text: text,fromWho: FromWho.me);
     messageList.add(newMessage);
     //Llamamos a setState pero con otro nombre
     notifyListeners();
+    moveScrollToBottom();
+  }
+  Future<void> moveScrollToBottom() async{
+    await Future.delayed(const Duration(milliseconds: 100));
+    chatScrollController.animateTo(
+      chatScrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOut);
   }
 }
